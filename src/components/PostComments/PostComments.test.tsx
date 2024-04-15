a9dc42f1789d11feea2f5f28619c4ef92af9be85
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-04-15 17:29:45 -0300

    Tarefa de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Teste de inserção de 2 comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-text'), {
+            target: {
+                value: 'Muito legal!',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-btn'));
+    
+        fireEvent.change(screen.getByTestId('comment-text'), {
+            target: {
+                value: 'Parabéns!',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-btn'));
+
+        expect(screen.getAllByTestId('comment-done')).toHaveLength(2);
+    });
 });
